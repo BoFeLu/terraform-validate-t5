77ebd2e80f12e4d97b04ccac6cab58d54df0abc3
BoFeLu(Alberto) <sg1apm@gmail.com>
2025-10-23 10:16:15 +0200

    fix(ci): añadir archivos Terraform y workflow funcional con LocalStack (T6)

---
+++ infra/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_access_key           = "test"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  description = "Nombre del bucket S3 simulado en LocalStack"
+  type        = string
+  default     = "t5-localstack-bucket"
+}
